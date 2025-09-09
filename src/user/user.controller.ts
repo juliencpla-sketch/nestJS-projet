a3c1c1b001b74a54d6c6ae06836c91aad678e403
julien-pla <julien.c.pla@gmail.com>
2025-09-09 12:08:10 +0200

    commit test 2

---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() Body: any) {
+    console.log('create user', Body);
+    return Body;
+  }
+}
